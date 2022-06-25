05403e4343cbcb1f34dd2970f7314e8f9ece3a66
Antony <theantonymars@gmail.com>
2022-06-25 19:56:25 +0300

    making changes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{io::{self, stdin}, path::{PathBuf, Path},fs, vec, collections::HashMap, process::exit, ffi::{OsStr, OsString}};
+use std::{io::{self, stdin}, path::{PathBuf, Path},fs, vec, collections::HashMap, process::exit, ffi::{OsString}};
+use std::io::{Write};
 
 use clap::{Parser, Subcommand};
 use unicase::UniCase; // this helps with case insensitivity
@@ -57,6 +58,7 @@ fn main() -> Result<(), io::Error>{
     let binaries = vec!["nano", "vi","vim", "nvim"];
     let args = Cli::parse();
 
+
     // global cheat path
     let home_dir = dirs::home_dir().unwrap();
     let cheat_folder = Path::new(&home_dir).join(".cheat");
@@ -65,6 +67,30 @@ fn main() -> Result<(), io::Error>{
     let mut files = read_files(cheat_folder)?; // getting the whole list of files in the directory OR an error
 
     let mut selector = vec![];
+
+
+    match config.editor_path.clone() {
+        Some(path) => {eprintln!("We have a path {:?}", path)},
+        None => { 
+            let mut editor_selection = String::new();
+            check_for_editor(binaries, &binary_base_path, &mut selector);
+            println!("Please select an editor");
+            for (index, editor) in selector.iter().enumerate() {
+                println!("{}) {:?} ", index, editor);
+            }
+            let mut editor_index = 0;
+            stdin().read_line(&mut editor_selection).ok().expect("An error occurred while capturing input");
+            match editor_selection.trim().parse::<usize>() {
+                Ok(num) => { editor_index = num },
+                Err(e) => eprintln!("An error occurred during casting: {}", e)
+            }
+            // println!("You selected {}", editor_index);
+            config.editor_path = Some(selector[editor_index].clone()); // setting the editor
+            println!("{:?}", &config);
+        }
+    }
+
+
     match args.command {
         Commands::L {  } => { 
             // let files_content = files.drain();
@@ -81,28 +107,6 @@ fn main() -> Result<(), io::Error>{
                 eprintln!("The file `{}` does not exist", &name);
                 exit(1);
             }
-            match config.editor_path.clone() {
-                Some(path) => {eprintln!("We have a path {:?}", path)},
-                None => { 
-                    let mut editor_selection = String::new();
-                    check_for_editor(binaries, &binary_base_path, &mut selector);
-                    println!("Please select an editor");
-                    for (index, editor) in selector.iter().enumerate() {
-                        println!("{}) {:?} ", index, editor);
-                    }
-                    let mut editor_index = 0;
-                    stdin().read_line(&mut editor_selection).ok().expect("An error occurred while capturing input");
-                    match editor_selection.trim().parse::<usize>() {
-                        Ok(num) => { editor_index = num },
-                        Err(e) => eprintln!("An error occurred during casting: {}", e)
-                    }
-                    // println!("You selected {}", editor_index);
-                    config.editor_path = Some(selector[editor_index].clone()); // setting the editor
-                    println!("{:?}", &config);
-                }
-            }
-
-            // eprintln!("{}", name);
             let p = &files.get(&UniCase::new(name)).unwrap().path;
             perform_edit(&config.editor_path.unwrap(), PathBuf::from(p));
         },
@@ -177,5 +181,8 @@ fn perform_edit(binary: &PathBuf, file_path: PathBuf) {
     let w = binary.to_string_lossy().to_string().to_owned();
     let bin = OsString::from(w);
 
-    std::process::Command::new(bin).arg(file_path).output().expect("An error occured while performing edit");
+    let cmd = std::process::Command::new(bin).args(&[
+        &file_path.to_string_lossy().as_ref()
+        ]).output().expect("An error occured while performing edit");
+    std::io::stdout().write_all(&cmd.stderr).unwrap();
 }
