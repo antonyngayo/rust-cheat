0c8c9260edbfb2fb0e500d936ad59587bda6d7b2
Jadz <info@groctech.com>
2023-11-07 12:12:55 +0200

    changing order of github actions

--- src/gitops/git.rs
+++ src/gitops/git.rs
@@ -3,23 +3,24 @@ use std::process::Command;
 #[allow(unused)]
 pub fn git_add(cheat_dir: &str, commit_msg: &str){
     let mut cmd = Command::new("git");
-    cmd.arg("-C").arg(cheat_dir).arg("add").arg(".");
-    eprintln!("[INFO] Adding files to git");
+    cmd.arg("-C").arg(cheat_dir).arg("fetch").arg("--prune").arg("origin");
+    eprintln!("[INFO] Fetching changes from remote");
     match cmd.output() {
         Ok(_) => {
-            cmd.arg("-C").arg(cheat_dir).arg("fetch").arg("--prune").arg("origin");
-            eprintln!("[INFO] Fetching changes from remote");
+            // merge if there are any changes
+            cmd.arg("-C").arg(cheat_dir).arg("merge").arg("origin/master");
+            eprintln!("[INFO] Merging changes from remote");
             match cmd.output() {
                 Ok(_) => {
-                    cmd.arg("-C").arg(cheat_dir).arg("pull").arg("--rebase").arg("origin");
-                    eprintln!("[INFO] Pulling changes from master");
+                    cmd.arg("-C").arg(cheat_dir).arg("add").arg("-A");
+                    eprintln!("[INFO] Adding files to git");
                     match cmd.output() {
                         Ok(_) => {
                             cmd.arg("-C").arg(cheat_dir).arg("commit").arg("-m").arg(commit_msg);
                             eprintln!("[INFO] Committing changes");
                             match cmd.output() {
                                 Ok(_) => {
-                                    cmd.arg("-C").arg(cheat_dir).arg("push");
+                                    cmd.arg("-C").arg(cheat_dir).arg("push").arg("origin").arg("master");
                                     eprintln!("[INFO] Pushing changes to remote");
                                     match cmd.output() {
                                         Ok(_) => println!("[INFO] Successfully pushed to remote"),
@@ -29,12 +30,12 @@ pub fn git_add(cheat_dir: &str, commit_msg: &str){
                                 Err(err) => println!("[ERROR] An error occured while committing files: {err}")
                             }
                         },
-                        Err(err) => println!("[ERROR] An error occured while fetching from remote: {err}")
+                        Err(err) => println!("[ERROR] An error occured while adding files: {err}")
                     }
                 },
-                Err(err) => println!("[ERROR] An error occured while fetching from remote: {err}")
+                Err(err) => println!("[ERROR] An error occured while merging repo: {err}")
             }
         }
-        Err(err) => println!("[ERROR] An error occured while adding files to git: {err}")
+        Err(err) => println!("[ERROR] An error occured while fetching files from origin master: {err}")
     }
 }
