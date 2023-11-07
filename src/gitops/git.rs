592ce42192a854161109c3699f21b2e97742e243
Jadz <info@groctech.com>
2023-11-07 12:01:53 +0200

    adding fetching, pulling, merging, and pushing into the git process

--- src/gitops/git.rs
+++ src/gitops/git.rs
@@ -4,26 +4,37 @@ use std::process::Command;
 pub fn git_add(cheat_dir: &str, commit_msg: &str){
     let mut cmd = Command::new("git");
     cmd.arg("-C").arg(cheat_dir).arg("add").arg(".");
-    let res = cmd.output();
-    match res {
+    eprintln!("[INFO] Adding files to git");
+    match cmd.output() {
         Ok(_) => {
-            let mut cmd = Command::new("git");
-            cmd.arg("-C").arg(cheat_dir).arg("commit").arg("-m").arg(commit_msg);
-            let res = cmd.output();
-            match res {
+            cmd.arg("-C").arg(cheat_dir).arg("fetch").arg("--prune").arg("origin");
+            eprintln!("[INFO] Fetching changes from remote");
+            match cmd.output() {
                 Ok(_) => {
-                    let mut cmd = Command::new("git");
-                    cmd.arg("-C").arg(cheat_dir).arg("push");
-                    let res = cmd.output();
-                    match res {
-                        Ok(_) => println!("Successfully pushed to remote"),
-                        Err(_) => println!("An error occured while pushing to remote")
+                    cmd.arg("-C").arg(cheat_dir).arg("pull").arg("--rebase").arg("origin");
+                    eprintln!("[INFO] Pulling changes from master");
+                    match cmd.output() {
+                        Ok(_) => {
+                            cmd.arg("-C").arg(cheat_dir).arg("commit").arg("-m").arg(commit_msg);
+                            eprintln!("[INFO] Committing changes");
+                            match cmd.output() {
+                                Ok(_) => {
+                                    cmd.arg("-C").arg(cheat_dir).arg("push");
+                                    eprintln!("[INFO] Pushing changes to remote");
+                                    match cmd.output() {
+                                        Ok(_) => println!("[INFO] Successfully pushed to remote"),
+                                        Err(err) => println!("[ERROR] An error occured while pushing to remote: {err}")
+                                    }
+                                },
+                                Err(err) => println!("[ERROR] An error occured while committing files: {err}")
+                            }
+                        },
+                        Err(err) => println!("[ERROR] An error occured while fetching from remote: {err}")
                     }
-                
                 },
-                Err(_) => println!("An error occured while committing files")
+                Err(err) => println!("[ERROR] An error occured while fetching from remote: {err}")
             }
         }
-        Err(_) => println!("An error occured while adding files to git")
+        Err(err) => println!("[ERROR] An error occured while adding files to git: {err}")
     }
 }
