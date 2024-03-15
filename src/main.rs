44dec4aa6fce282bc93e3f30f7309bcacaa8dc56
Jadz <info@groctech.com>
2024-03-15 22:19:17 +0200

    adding all files

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,45 @@
-use std::{io::{self}, path::{PathBuf, Path},vec, process::exit, fs, env};
+use clap::{Parser, Subcommand};
 use serde_json;
+use std::path::{Path, PathBuf};
+use std::{
+    fs,
+    io::{self},
+    vec,
+};
 use unicase::UniCase; // this helps with case insensitivity
 mod configuration;
-mod utils;
-mod tries;
 mod gitops;
-
-use tries::TrieStructure;
-
-use utils::{create_config, perform_edit, read_files, choose_editor, perform_text_dump};
+mod tries;
+mod utils;
 
 use crate::configuration::Config;
+use tries::TrieStructure;
+use utils::{choose_editor, create_config, perform_edit, perform_text_dump, read_files};
 
-fn main() -> Result<(), io::Error>{
+#[derive(Parser, Debug)]
+#[clap(author = "Groctech", version, about = "Cheet sheet manager")]
+struct Args {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+// thinking of introducing Clap for command line arguments
+#[derive(Subcommand, Debug)]
+enum Command {
+    Init,
+    List,
+    Edit {
+        #[clap(short = 'e', long = "edit")]
+        file_name: String,
+    },
+    Read {
+        #[clap(short = 'r', long = "read")]
+        file_name: String,
+    },
+}
+
+// clap struct
+fn main() -> Result<(), io::Error> {
     // global variables
     let mut config = configuration::Config::new();
     let home_dir = dirs::home_dir().unwrap();
@@ -20,95 +47,81 @@ fn main() -> Result<(), io::Error>{
     let files = read_files(&cheat_folder)?; // getting the whole list of files in the directory OR an error
     let mut selector = vec![];
     let binary_base_path = PathBuf::from("/usr/bin/");
-    let binaries = vec!["nano", "vi","vim", "nvim", "emacs", "ee"];
-    // getting commandline arguments 
-    let cli_args: Vec<String> = env::args().collect();
-    if cli_args.len() == 0 {
-        println!("too few arguments");
-    }
-    // creating a config file if it did not exist 
-    match create_config(&config){
-        (true, path) => {
-            // loading configs into program
-            match fs::read_to_string(&path) {
-                Ok(content) => {
-                    let conf_text: Config = serde_json::from_str(&content).unwrap();
-                    config = conf_text;
-                },
-                Err(_) => eprintln!("The config file is empty")
+    let binaries = vec!["nano", "vi", "vim", "nvim", "emacs", "ee"];
+
+    // use clap
+    let args = Args::parse();
+
+    match args.command {
+        Command::Init => {
+            match create_config(&config) {
+                (true, path) => {
+                    // loading configs into program
+                    match fs::read_to_string(&path) {
+                        Ok(content) => {
+                            let conf_text: Config = serde_json::from_str(&content).unwrap();
+                            config = conf_text;
+                        }
+                        Err(_) => eprintln!("The config file is empty"),
+                    }
+                    config.config_path = path.to_string_lossy().to_string().to_owned(); // setting the config path to be written to disk soon
+                    fs::write(path, serde_json::to_string_pretty(&config).unwrap()).unwrap();
+                }
+                (false, _) => eprintln!("An error occured while creating config file"),
             }
-            config.config_path = path.to_string_lossy().to_string().to_owned(); // setting the config path to be written to disk soon
-            fs::write(path, serde_json::to_string_pretty(&config).unwrap()).unwrap();
+            match &config.editor_path {
+                Some(editor) => {
+                    if !editor.exists() {
+                        choose_editor(binaries, &binary_base_path, &mut selector, &mut config)
+                    }
+                } // checks whether the editor path is set in json file
+                None => choose_editor(binaries, &binary_base_path, &mut selector, &mut config), // assumes editor not set in json and forces a set by user
+            }
+
+            // implementing the trie structure for quick file search
+
+            let mut file_name_list = TrieStructure::new();
+            for file_name in &files {
+                file_name_list.insert(file_name.0.to_string());
+            }
+        }
+        Command::List => {
+            for file in &files {
+                // looping through HashMap contents
+                println!(
+                    "{:indent$} {}",
+                    &file.1.name,
+                    &file.1.path.to_string(),
+                    indent = 40
+                );
+            }
+        }
+        Command::Edit { file_name } => {
+            if !files.contains_key(&UniCase::new(file_name.clone())) {
+                // performing a `LOOKUP` in the HashMap for fast search
+                // check whether file exists; if not, we create a new one with the prescribed name
+                let new_file = Path::new(&cheat_folder).join(&file_name); // preparing file name
+                let result = fs::File::create(&new_file); // creating the file
+                match result {
+                    // checking whether the create function succeeded
+                    Ok(_) => perform_edit(&config.editor_path.as_ref().unwrap(), new_file), // if operation succeeds,
+                    Err(_) => eprintln!("An error occured while creating file"),
+                }
+            } else {
+                let file_path =
+                    PathBuf::from(files.get(&UniCase::new(file_name)).unwrap().path.clone());
+                perform_edit(&config.editor_path.as_ref().unwrap(), file_path);
+            }
+        }
+        Command::Read { file_name } => match files.get(&UniCase::new(file_name.clone())) {
+            None => {
+                eprintln!("The file does not exist");
+                std::process::exit(1);
+            }
+            Some(file_name) => {
+                perform_text_dump(&PathBuf::from(&file_name.path));
+            }
         },
-        (false, _) => eprintln!("An error occured while creating config file")
     }
-    match &config.editor_path {
-        Some(editor) => { if !editor.exists(){ choose_editor(binaries, &binary_base_path, &mut selector, &mut config) } }, // checks whether the editor path is set in json file
-        None => { choose_editor(binaries, &binary_base_path, &mut selector, &mut config) } // assumes editor not set in json and forces a set by user
-    }
-
-    // implementing the trie structure for quick file search
-
-    let mut file_name_list = TrieStructure::new();
-    for file_name in &files {
-        file_name_list.insert(file_name.0.to_string());
-    }
-
-    // parsing command line arguments
-    if cli_args.len() < 2 {
-        println!("There are no arguments passed"); // put a menu
-        println!();
-        println!("      == USAGE ==         ");
-        println!();
-        println!("  -l  -   List all files  ");
-        println!();
-        println!("  -e  -   Edit a file     ");
-        println!();
-        println!("  -s  -   Search a file   ");
-        println!();
-        println!("  -d  -   Delete a file   ");
-        println!();
-        println!("  -p  [Commit Message ]-   Push to git ");
-        println!();
-        exit(1);
-    }
-    if cli_args.len() == 2 && cli_args[1] == "-l" { // for listing all the file names in the .cheat folder
-        for file in &files { // looping through HashMap contents
-            println!("{:indent$} {}", &file.1.name, &file.1.path.to_string(), indent=40);
-        }
-    }else if cli_args[1] == "-s" && cli_args.len() == 3  { // for searching file names and returns a boolean for now
-        let (_, partal_filename) = file_name_list.find(cli_args[2].to_string());
-        println!("Partial search: {}", partal_filename);
-    }else if cli_args[1] == "-e" && cli_args.len() == 3 { // for editing a file name
-        if !files.contains_key(&UniCase::new(cli_args[2].clone())) { // performing a `LOOKUP` in the HashMap for fast search
-            // check whether file exists; if not, we create a new one with the prescribed name
-            let new_file = Path::new(&cheat_folder).join(&cli_args[2]); // preparing file name
-            let result = fs::File::create(&new_file); // creating the file
-            match result { // checking whether the create function succeeded
-                Ok(_) => perform_edit(&config.editor_path.unwrap(), new_file), // if operation succeeds, we open for editing
-                Err(error) => eprintln!("An error occurred when creating file: {}", error)
-            }
-            exit(1);
-        }
-        let p = &files.get(&UniCase::new(cli_args[2].clone())).unwrap().path; // checking whether the file exists in a CASE INSESNITIVE manner
-        perform_edit(&config.editor_path.unwrap(), PathBuf::from(p)); // opening file for edit
-    }else if cli_args[1] == "-d" && cli_args.len() == 3 {
-        let file_to_delete = &files.get(&UniCase::new(cli_args[2].clone())).unwrap().path;
-        let d_res = fs::remove_file(PathBuf::from(file_to_delete));
-        match d_res {
-            Ok(_) => eprintln!("Deleted the file: {}", &file_to_delete),
-            Err(err) => eprintln!("Error deleting file {}: {}", &file_to_delete, err)
-        }     
-    }
-    else if cli_args[1] == "-p" && cli_args.len() == 3 {
-        println!("Pushing to git");
-        gitops::git::git_add(&cheat_folder.to_string_lossy().to_string(), &cli_args[2].clone());
-    } 
-    else{ // dumps contents of a file onto the screen
-        if Path::new(&cheat_folder).join(&cli_args[1].clone()).exists() { // checks if file exists
-            print!("{}", perform_text_dump(&PathBuf::from(&files.get(&UniCase::new(cli_args[1].clone())).unwrap().path))); // if it exists, we get it and unwrap it
-        }else{ eprintln!("The file `{}` does not exist", &cli_args[1].clone()) } // we print out an error
-    }
-
     Ok(())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,9 @@
-use std::{path::{Path, PathBuf}, fs, collections::HashMap, io::{self, stdin}};
+use std::{
+    collections::HashMap,
+    fs,
+    io::{self, stdin},
+    path::{Path, PathBuf},
+};
 
 use unicase::UniCase;
 
@@ -7,17 +12,23 @@ use crate::configuration::Config;
 #[derive(Debug, Eq, PartialEq, PartialOrd, Ord)] // this allows the structure to be Order-able and
 pub struct FileNames {
     pub name: String,
-    pub path: String
+    pub path: String,
 }
 impl FileNames {
     pub fn new(p: PathBuf) -> Self {
-        Self { name: p.to_string_lossy().split("/").last().unwrap().to_string(), path: p.to_string_lossy().to_string() }
+        Self {
+            name: p.to_string_lossy().split("/").last().unwrap().to_string(),
+            path: p.to_string_lossy().to_string(),
+        }
     }
 }
-pub fn read_files(cheat_folder: &PathBuf) -> Result<HashMap<UniCase<String>, FileNames>, io::Error> { // returning a vector on success or an error on failure 
+pub fn read_files(
+    cheat_folder: &PathBuf,
+) -> Result<HashMap<UniCase<String>, FileNames>, io::Error> {
+    // returning a vector on success or an error on failure
     // setting home dir as global variable
     let home_dir = dirs::home_dir().unwrap();
-    // create a path to .cheat 
+    // create a path to .cheat
     let cheat_path = match &cheat_folder.exists() {
         true => Path::new(&home_dir).join(".cheat"), // setting the path appropriately
         false => {
@@ -26,66 +37,89 @@ pub fn read_files(cheat_folder: &PathBuf) -> Result<HashMap<UniCase<String>, Fil
                 // if successful, we have created the .chat folder
                 Ok(_) => Path::new(&home_dir).join(".cheat"),
                 // an error occured and we set an empty path
-                Err(_) => { PathBuf::new() }
+                Err(_) => PathBuf::new(),
             }
         }
     };
     let target_folder = std::fs::read_dir(cheat_path)?; // passing the `.cheat` psth to be read and enumerated
     let mut res = HashMap::new(); // declaring the vec object
-    // name to be used as key in the hashmap
-    for (_, file) in target_folder.enumerate(){  
-        let hname = file.as_ref().unwrap().path().to_string_lossy().split("/").last().unwrap().to_string();
+                                  // name to be used as key in the hashmap
+    for (_, file) in target_folder.enumerate() {
+        let hname = file
+            .as_ref()
+            .unwrap()
+            .path()
+            .to_string_lossy()
+            .split("/")
+            .last()
+            .unwrap()
+            .to_string();
         res.insert(UniCase::new(hname), FileNames::new(file.unwrap().path())); // pushing the paths into the vector
     }
     return Ok(res);
 }
 
-
 // create a config file
 pub fn create_config(config: &Config) -> (bool, PathBuf) {
-    // creating a config file for the cheat binary 
+    // creating a config file for the cheat binary
     let home_dir = dirs::home_dir().unwrap();
-    match Path::new(&home_dir).join(".cheat.json").exists(){
-        true => { return (true, Path::new(&home_dir).join(".cheat.json")) },
+    match Path::new(&home_dir).join(".cheat.json").exists() {
+        true => return (true, Path::new(&home_dir).join(".cheat.json")),
         false => {
-            let path =  Path::new(&home_dir).join(".cheat.json");
+            let path = Path::new(&home_dir).join(".cheat.json");
             let res = fs::File::create(&path);
             // filling the config file with the initial configurations so the file has something
             fs::write(path, serde_json::to_string_pretty(&config).unwrap()).unwrap();
             match res {
                 Ok(_) => return (true, Path::new(&home_dir).join(".cheat.json")),
-                Err(_) => return (false, PathBuf::new())
+                Err(_) => return (false, PathBuf::new()),
             }
-        },
+        }
     };
 }
 
 // checks whether the editor binary exists in the OS
-pub fn check_for_editor(binaries: Vec<&str>, binary_base_path: &PathBuf, selector: &mut Vec<PathBuf>){
+pub fn check_for_editor(
+    binaries: Vec<&str>,
+    binary_base_path: &PathBuf,
+    selector: &mut Vec<PathBuf>,
+) {
     for bin in binaries {
         match Path::new(&binary_base_path).join(bin).exists() {
             true => {
                 selector.push(binary_base_path.join(bin));
-            },
-            false => {continue;}
+            }
+            false => {
+                continue;
+            }
         }
     }
 }
 // open the files using the chosen editor; bash does the terminal spawning under the hood
 pub fn perform_edit(binary: &PathBuf, file_path: PathBuf) {
-    //// when starting the SHELL, you need to spawn a binary then wait for it to finish execution. That is why we are using `spawn` and `wait` 
-    let _ = std::process::Command::new(binary).args(&[
-        &file_path.to_string_lossy().as_ref()
-        ]).spawn().expect("An error occured while performing edit").wait();
+    //// when starting the SHELL, you need to spawn a binary then wait for it to finish execution. That is why we are using `spawn` and `wait`
+    let _ = std::process::Command::new(binary)
+        .args(&[&file_path.to_string_lossy().as_ref()])
+        .spawn()
+        .expect("An error occured while performing edit")
+        .wait();
 }
 // open the files using the chosen editor; bash does the terminal spawning under the hood
-pub fn perform_text_dump(file_path: &PathBuf) -> String {
-    let content = fs::read_to_string(&file_path).unwrap();
-    return content
+pub fn perform_text_dump(file_path: &PathBuf) {
+    let f = fs::read(&file_path);
+    // reading the file and writing it to the terminal
+    std::io::Write::write_all(&mut std::io::stdout(), &f.unwrap()).unwrap();
+    // flushing the buffer
+    std::io::Write::flush(&mut std::io::stdout()).unwrap();
 }
 
 // allows user to choose the editor of their choice and saves it in the config file
-pub fn choose_editor(binaries: Vec<&str>, binary_base_path: &PathBuf, selector: &mut Vec<PathBuf>, config: &mut Config) {
+pub fn choose_editor(
+    binaries: Vec<&str>,
+    binary_base_path: &PathBuf,
+    selector: &mut Vec<PathBuf>,
+    config: &mut Config,
+) {
     let mut editor_selection = String::new();
     check_for_editor(binaries, &binary_base_path, selector);
     println!("Please select an editor");
@@ -93,11 +127,22 @@ pub fn choose_editor(binaries: Vec<&str>, binary_base_path: &PathBuf, selector: 
         println!("{}) {:?} ", index + 1, editor); // lists out all the editors found in the system
     }
     let mut editor_index = 0; // initializing the variable
-    stdin().read_line(&mut editor_selection).ok().expect("An error occurred while capturing input"); // getting input from user
+    stdin()
+        .read_line(&mut editor_selection)
+        .ok()
+        .expect("An error occurred while capturing input"); // getting input from user
     match editor_selection.trim().parse::<usize>() {
-        Ok(num) => { if num < selector.len() + 1 { editor_index = num - 1 } },
-        Err(e) => eprintln!("An error occurred during casting: {}", e)
+        Ok(num) => {
+            if num < selector.len() + 1 {
+                editor_index = num - 1
+            }
+        }
+        Err(e) => eprintln!("An error occurred during casting: {}", e),
     }
     config.editor_path = Some(selector[editor_index].clone()); // setting the editor
-    fs::write(&config.config_path, serde_json::to_string_pretty(&config).unwrap()).unwrap();
+    fs::write(
+        &config.config_path,
+        serde_json::to_string_pretty(&config).unwrap(),
+    )
+    .unwrap();
 }
